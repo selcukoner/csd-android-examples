e3596893e3dbebf9b28e290c3db068371cfa6bda
Selçuk Öner <cihanselcukoner@gmail.com>
2023-11-07 23:09:03 +0300

    add time clock using thread

--- 009-DisplayDateTime - Timer/app/src/main/java/org/cso/android/app/displaydatetime/MainActivity.kt
+++ 009-DisplayDateTime - Timer/app/src/main/java/org/cso/android/app/displaydatetime/MainActivity.kt
@@ -7,26 +7,53 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import org.cso.android.app.displaydatetime.databinding.ActivityMainBinding
 import org.csystem.android.util.datetime.di.module.formatter.annotation.LocalDateTimeFormatterInterceptor
+import org.csystem.android.util.datetime.di.module.formatter.annotation.LocalTimeFormatterInterceptor
 import java.time.LocalDateTime
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.util.Timer
 import java.util.TimerTask
 import javax.inject.Inject
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
-    private lateinit var mTimer: Timer
+    private lateinit var mTimerDateTime: Timer
+    private lateinit var mClockThread : Thread
 
-    private fun createTimerTask() = object : TimerTask(){
+    private fun createDateTimeTimerTask() = object : TimerTask(){
         override fun run() {
-            mBinding.dateTime = formatter.format(LocalDateTime.now())
+            mBinding.dateTime = dateTimeFormatter.format(LocalDateTime.now())
         }
+    }
+
+    private fun clockThreadCallback()
+    {
+        timeFormatter = DateTimeFormatter.ofPattern("kk:mm:ss")
+        while (true){ // usage of Timer is better
+            mBinding.time = timeFormatter.format(LocalTime.now())
+            Thread.sleep(1000)
+        }
+    }
+    private fun startClock()
+    {
+     mClockThread = thread { clockThreadCallback() }
+    }
+
+    private fun scheduleDateTimeTimer()
+    {
+        mTimerDateTime = Timer()
+        dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy kk:mm:ss")
+        mTimerDateTime.scheduleAtFixedRate(createDateTimeTimerTask(),0, 1000)
     }
 
     @Inject
     @LocalDateTimeFormatterInterceptor
-    lateinit var formatter : DateTimeFormatter
+    lateinit var dateTimeFormatter : DateTimeFormatter
+    @Inject
+    @LocalTimeFormatterInterceptor
+    lateinit var timeFormatter : DateTimeFormatter
 
     private fun initBinding()
     {
@@ -48,9 +75,8 @@ class MainActivity : AppCompatActivity() {
         try{
 
             super.onStart()
-            mTimer = Timer()
-            formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy kk:mm:ss")
-            mTimer.scheduleAtFixedRate(createTimerTask(),0, 1000)
+            scheduleDateTimeTimer()
+            startClock()
         }
         catch (ex: Throwable){
             Log.d("on-start", ex.message!!)
@@ -60,7 +86,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStop() {
         try {
-            mTimer.cancel()
+            mTimerDateTime.cancel()
+            mClockThread.interrupt()
             super.onStop()
 
         }
@@ -69,8 +96,5 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Problem occurred on stop", Toast.LENGTH_LONG).show()
         }
     }
-
-
-
 
 }
